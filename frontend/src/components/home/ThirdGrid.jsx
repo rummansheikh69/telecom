0eb8e59099487750a35de06943deafb85efb224d
rummansheikh69 <rummansheikh2004gmail.com>
2026-06-19 03:11:23 +0600

    same

--- frontend/src/components/home/ThirdGrid.jsx
+++ frontend/src/components/home/ThirdGrid.jsx
@@ -6,28 +6,25 @@ import { FiPlus } from "react-icons/fi";
 import { GoStarFill } from "react-icons/go";
 import { TbMailFilled } from "react-icons/tb";
 import { FaCartShopping } from "react-icons/fa6";
+import { TbClipboardListFilled } from "react-icons/tb";
 
 function ThirdGrid() {
   return (
     <div className="mt-6 grid grid-cols-5 gap-2">
-      {/* doc */}
+      {/* summery */}
       <div className="bg-subMain rounded-2xl p-2 pt-3 shadow-lg flex items-center justify-center flex-col gap-1.5">
-        <div className=" size-8">
-          <img
-            src="images/doc.svg"
-            alt="wallet"
-            className=" w-full h-full object-contain"
-          />
+        <div className="  flex items-center justify-center">
+          <TbClipboardListFilled size={34} className=" text-secondary" />
         </div>
-        <p className="text-sm text-primary font-medium">Rules</p>
+        <p className="text-sm text-primary font-medium">Summery</p>
       </div>
 
-      {/* Verify */}
+      {/* orders */}
       <div className="bg-subMain rounded-2xl p-2 pt-3 shadow-lg flex items-center justify-center flex-col gap-1.5">
         <div className=" size-8 flex items-center justify-center">
-          <MdVerifiedUser size={32} className=" text-secondary" />
+          <FaCartShopping size={32} className=" text-secondary" />
         </div>
-        <p className="text-sm text-primary font-medium">Verify</p>
+        <p className="text-sm text-primary font-medium">Orders</p>
       </div>
 
       {/* Officer */}
@@ -46,12 +43,12 @@ function ThirdGrid() {
         <p className="text-sm text-primary font-medium">Chat</p>
       </div>
 
-      {/* helpline */}
+      {/* inbox */}
       <div className="bg-subMain rounded-2xl p-2 pt-3 shadow-lg flex items-center justify-center flex-col gap-1.5">
         <div className=" size-8 flex items-center justify-center">
-          <PiPhoneCallFill size={32} className=" text-secondary" />
+          <TbMailFilled size={32} className=" text-secondary" />
         </div>
-        <p className="text-sm text-primary font-medium">Helpline</p>
+        <p className="text-sm text-primary font-medium">Inbox</p>
       </div>
 
       {/* admission */}
@@ -62,40 +59,40 @@ function ThirdGrid() {
         <p className="text-sm text-primary font-medium">Admit</p>
       </div>
 
-      {/* balance */}
+      {/* Verify */}
+      <div className="bg-subMain rounded-2xl p-2 pt-3 shadow-lg flex items-center justify-center flex-col gap-1.5">
+        <div className=" size-8 flex items-center justify-center">
+          <MdVerifiedUser size={32} className=" text-secondary" />
+        </div>
+        <p className="text-sm text-primary font-medium">Verify</p>
+      </div>
+
+      {/* helpline */}
+      <div className="bg-subMain rounded-2xl p-2 pt-3 shadow-lg flex items-center justify-center flex-col gap-1.5">
+        <div className=" size-8 flex items-center justify-center">
+          <PiPhoneCallFill size={32} className=" text-secondary" />
+        </div>
+        <p className="text-sm text-primary font-medium">Helpline</p>
+      </div>
+
+      {/* rules */}
       <div className="bg-subMain rounded-2xl p-2 pt-3 shadow-lg flex items-center justify-center flex-col gap-1.5">
         <div className=" size-8">
           <img
-            src="images/money.svg"
+            src="images/doc.svg"
             alt="wallet"
             className=" w-full h-full object-contain"
           />
         </div>
-        <p className="text-sm text-primary font-medium">Balance</p>
+        <p className="text-sm text-primary font-medium">Rules</p>
       </div>
 
-      {/* admission */}
+      {/* reviews */}
       <div className="bg-subMain rounded-2xl p-2 pt-3 shadow-lg flex items-center justify-center flex-col gap-1.5">
         <div className=" size-8 flex items-center justify-center">
           <GoStarFill size={32} className=" text-secondary" />
         </div>
         <p className="text-sm text-primary font-medium">Reviews</p>
-      </div>
-
-      {/* inbox */}
-      <div className="bg-subMain rounded-2xl p-2 pt-3 shadow-lg flex items-center justify-center flex-col gap-1.5">
-        <div className=" size-8 flex items-center justify-center">
-          <TbMailFilled size={32} className=" text-secondary" />
-        </div>
-        <p className="text-sm text-primary font-medium">Inbox</p>
-      </div>
-
-      {/* orders */}
-      <div className="bg-subMain rounded-2xl p-2 pt-3 shadow-lg flex items-center justify-center flex-col gap-1.5">
-        <div className=" size-8 flex items-center justify-center">
-          <FaCartShopping size={32} className=" text-secondary" />
-        </div>
-        <p className="text-sm text-primary font-medium">Orders</p>
       </div>
     </div>
   );
